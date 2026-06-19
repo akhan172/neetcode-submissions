cc9977f97f89cd5d27635ffbbdae1cb63bd08077
MD ABRAR KHAN <70259069+akhan172@users.noreply.github.com>
2026-06-20 01:36:43 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        // Find middle
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        ListNode prev = null, curr = slow.next;
+        slow.next = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Merge
+        ListNode l1 = head, l2 = prev;
+        while (l2 != null) {
+            ListNode n1 = l1.next;
+            ListNode n2 = l2.next;
+
+            l1.next = l2;
+            l2.next = n1;
+
+            l1 = n1;
+            l2 = n2;
+        }
+    }
+}
